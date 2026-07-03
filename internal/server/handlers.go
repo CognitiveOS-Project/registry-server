0818522454d3c52d5475238d07866e7889854312
Jean Machuca <jeanmachuca@users.noreply.github.com>
2026-07-03 18:20:24 -0400

    fix: resolve all golangci-lint errcheck issues (#28)

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -201,5 +201,5 @@ func (s *Server) handleUnlock() http.HandlerFunc {
 func writeJSON(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+	_ = json.NewEncoder(w).Encode(v)
 }
